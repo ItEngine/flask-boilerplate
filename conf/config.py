7f444cdc85c51131c7f56b124e3220adc0d0859a
Martin Peveri <martinpeveri@gmail.com>
2016-06-16 08:22:36 -0300

    Config local configuration

--- conf/config.py
+++ conf/config.py
@@ -1,22 +1,7 @@
-import os
+from .config_local import *
 
-# Grabs the folder where the script runs.
-BASEDIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
-
-# Enable debug mode.
-DEBUG = True
-
-# Secret key for session management. You can generate random strings here:
-# http://clsc.net/tools-old/random-string-generator.php
-SECRET_KEY = 'my-precious-secret-key'
-
-# Connect to the database
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'database.db')
+# Flask-SQLAlchemy will track modifications of objects and emit signals
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 # Enable protection agains *Cross-site Request Forgery (CSRF)*
 CSRF_ENABLED = True
-
-# Use a secure, unique and absolutely secret key for
-# signing the data.
-CSRF_SESSION_KEY = "secret-csrf-flask-app"
