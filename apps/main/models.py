d163e116fcd0f9eb227e15efd52c16629aad86d1
Martín Peveri <martinpeveri@gmail.com>
2017-03-14 18:28:12 -0300

    Update model user

--- apps/main/models.py
+++ apps/main/models.py
@@ -16,7 +16,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
     first_name = db.Column(db.String(120), nullable=False)
     last_name = db.Column(db.String(120), nullable=False)
     date_join = db.Column(
